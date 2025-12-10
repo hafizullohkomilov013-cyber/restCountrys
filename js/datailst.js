aff8a677582f91642b8993fef6b1801b662d14e1
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2025-12-10 22:59:48 +0500

    tugattim lekin chalsi bor

--- js/datailst.js
+++ js/datailst.js
@@ -9,15 +9,3 @@ let findArr = countries.find((item) => item.id === id);
 
 console.log(findArr);
 
-detels.innerHTML = `
-  <img src="${findArr.flag}" alt="${findArr.name}" />
-  <h2>${findArr.name}</h2>
-
-  <p><strong>Population :</strong> ${findArr.population}</p>
-  <p><strong>Region :</strong> ${findArr.region}</p>
-  <p><strong>Capital :</strong> ${findArr.capital}</p>
-  <p><strong>Languages :</strong> ${findArr.languages.join(", ")}</p>
-  <p><strong>Borders :</strong> ${
-    findArr.borders?.length ? findArr.borders.join(", ") : "No borders"
-  }</p>
-`;
